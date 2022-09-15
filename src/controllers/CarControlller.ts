18561605ad662c12ec648b5e0fbfa979ef9e06f1
Thiago Medeiros <medeiroshs@hotmail.com>
2022-09-15 15:45:13 -0300

    feat: criando o del e update de controller

--- src/controllers/CarControlller.ts
+++ src/controllers/CarControlller.ts
@@ -18,6 +18,16 @@ class CarController {
     const result = await this.carService.readOne(req.params.id);
     res.status(200).json(result);
   }
+
+  public async update(req: Request, res: Response) {
+    const result = await this.carService.update(req.params.id, req.body);
+    res.status(200).json(result);
+  }
+
+  public async delete(req: Request, res: Response) {
+    await this.carService.delete(req.params.id);
+    res.sendStatus(204);
+  }
 }
 
 export default CarController;
