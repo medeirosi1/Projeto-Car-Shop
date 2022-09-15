4bf9d283650a0fe47ad837bfb82cd7c572ab6781
Thiago Medeiros <medeiroshs@hotmail.com>
2022-09-15 13:09:18 -0300

    feat: criando os get de car no controller

--- src/controllers/CarControlller.ts
+++ src/controllers/CarControlller.ts
@@ -8,6 +8,16 @@ class CarController {
     const result = await this.carService.create(req.body);
     res.status(201).json(result);
   }
+  
+  public async read(req: Request, res: Response) {
+    const result = await this.carService.read();
+    res.status(200).json(result);
+  }
+
+  public async readOne(req: Request, res: Response) {
+    const result = await this.carService.readOne(req.params.id);
+    res.status(200).json(result);
+  }
 }
 
 export default CarController;
