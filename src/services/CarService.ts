87076c68bcb6e985b31a24fe1703462792ff9d00
Thiago Medeiros <medeiroshs@hotmail.com>
2022-09-15 12:08:15 -0300

    feat: criando o service Car

---
+++ src/services/CarService.ts
@@ -0,0 +1,37 @@
+import ICar, { carSchema } from '../interfaces/ICar';
+import IModel from '../interfaces/IModel';
+import IService from '../interfaces/IService';
+
+class CarService implements IService<ICar> {
+  private _car: IModel<ICar>;
+
+  constructor(car: IModel<ICar>) {
+    this._car = car;
+  }
+  public async create(obj: ICar): Promise<ICar> {
+    const parsed = carSchema.safeParse(obj);
+    if (!parsed.success) {
+      throw parsed.error;
+    }
+    const car = await this._car.create(parsed.data);
+    return car;
+  }
+  public async read(): Promise<ICar[]> {
+    const car = await this._car.read();
+    return car;
+  }
+  public async readOne(_id: string): Promise<ICar | null> {
+    const car = this._car.readOne(_id);
+    return car;
+  }
+  public async update(_id: string, obj: ICar): Promise< ICar | null> {
+    const car = this._car.update(_id, obj);
+    return car;
+  }
+  public async delete(_id: string): Promise< ICar | null> {
+    const car = this._car.delete(_id);
+    return car;
+  }
+}
+
+export default CarService;
