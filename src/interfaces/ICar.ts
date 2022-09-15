a2a755cb118bbd0cb17b4de9212227be41d5c667
Thiago Medeiros <medeiroshs@hotmail.com>
2022-09-15 13:43:06 -0300

    feat: adicionando as mensagens de erro na interface

--- src/interfaces/ICar.ts
+++ src/interfaces/ICar.ts
@@ -2,8 +2,14 @@ import { z } from 'zod';
 import { vehicleSchema } from './IVehicle';
 
 const carSchema = vehicleSchema.extend({
-  doorsQty: z.number().int().min(2).max(5),
-  seatsQty: z.number().int().min(2).max(8),
+  doorsQty: z.number({
+    required_error: 'Doors quantity is required',
+    invalid_type_error: 'Doors quantity must be a number',
+  }).int().min(2).max(4),
+  seatsQty: z.number({
+    required_error: 'Seats quantity is required',
+    invalid_type_error: 'Seats quantity must be a number',  
+  }).int().min(2).max(7),
 });
 
 type ICar = z.infer<typeof carSchema>;
