058b050f8c38f33a35aeb0d0f1cca2e656d925cf
Thiago Medeiros <medeiroshs@hotmail.com>
2022-09-15 14:54:01 -0300

    feat: criando o teste da camada controller

---
+++ src/tests/unit/controllers/Car.test.ts
@@ -0,0 +1,59 @@
+import { expect } from 'chai';
+import * as sinon from 'sinon';
+import { NextFunction, Request, Response } from 'express';
+import Cars from '../../../models/CarModel';
+import CarService from '../../../services/CarService';
+import CarController from '../../../controllers/CarControlller';
+import { carMockCreate, carMockwithId } from '../../mocks/carMock';
+
+
+describe('Car Controller', () => {
+    const carModel = new Cars();
+    const carService = new CarService(carModel);
+    const carController = new CarController(carService);
+    const req = {} as Request;
+    const res = {} as Response;
+
+    before(() => {
+        sinon.stub(carService, 'create').resolves(carMockCreate);
+        sinon.stub(carService, 'read').resolves([carMockCreate]);
+        sinon.stub(carService, 'readOne').resolves(carMockCreate);
+    
+        res.status = sinon.stub().returns(res);
+        res.json = sinon.stub().returns(res);
+      });
+    
+      after(() => {
+        sinon.restore()
+      })
+
+      describe('Criando um Carro', () => {
+        it('Criado com Sucesso', async () => {
+            req.body = carMockCreate;
+            await carController.create(req, res);
+
+            expect((res.status as sinon.SinonStub).calledWith(201)).to.be.true;
+            expect((res.json as sinon.SinonStub).calledWith(carMockCreate)).to.be.true;
+        });
+      });
+
+      describe('Lendo todos os carros', () => {
+        it('Criado com Sucesso', async () => {
+            await carController.read(req, res);
+
+            expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
+            expect((res.json as sinon.SinonStub).calledWith(carMockCreate)).to.be.true;
+        });
+      });
+
+      describe('Lendo um Carro', () => {
+        it('Criado com Sucesso', async () => {
+            req.params = { id: carMockwithId._id };
+            await carController.readOne(req, res);
+
+            expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
+            expect((res.json as sinon.SinonStub).calledWith(carMockCreate)).to.be.true;
+        });
+      });
+
+})
