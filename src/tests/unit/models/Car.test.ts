f90b8258a3e117172d2a41dc6bc8aab7569b29c2
Thiago Medeiros <medeiroshs@hotmail.com>
2022-09-15 14:54:40 -0300

    feat: teste do read e readone da camada model

--- src/tests/unit/models/Car.test.ts
+++ src/tests/unit/models/Car.test.ts
@@ -12,7 +12,7 @@ describe('Car Model', () => {
     before(() => {
         sinon.stub(Model, 'create').resolves(carMockwithId);
         sinon.stub(Model, 'find').resolves([carMockwithId]);
-        // sinon.stub(Model, 'findOne').resolves();
+        sinon.stub(Model, 'findOne').resolves(carMockwithId);
         // sinon.stub(Model, 'findByIdAndUpdate').resolves();
         // sinon.stub(Model, 'findByIdAndDelete').resolves();
     })
@@ -22,10 +22,34 @@ describe('Car Model', () => {
     });
 
     describe('Criando um Carro', () => {
-        it.only('Criado com Sucesso', async () => {
+        it('Criado com Sucesso', async () => {
             const car = await carModel.create(carMockCreate);
             expect(car).to.be.an('object');
             expect(car).to.be.deep.equal(carMockwithId);
+        });
+    });
+
+    describe('Listando todos os carros', () => {
+        it('Criado com Sucesso', async () => {
+            const car = await carModel.read();
+            expect(car).to.be.an('array');
+            expect(car).to.be.deep.equal([carMockwithId]);
+        });
+    });
+
+    describe('Listando um Carro', () => {
+        it('Criado com Sucesso', async () => {
+            const car = await carModel.readOne('63235cd68418bba5d1d401da');
+            // expect(car).to.be.an('object');
+            expect(car).to.be.deep.equal(carMockwithId);
+        });
+
+        it('id possua menos 24 caracteres', async () => {
+            try {
+                await carModel.readOne('63235cd68418bba5d1d401');
+            } catch (error: any) {
+                expect(error.message).to.be.deep.equal(ErrorTypes.InvalidMongoId);
+            }
         });
     })
 })
