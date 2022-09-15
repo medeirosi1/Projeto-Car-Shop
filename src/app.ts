8a579a4cab144b0e6a4330576550ef7d66e89cb6
Thiago Medeiros <medeiroshs@hotmail.com>
2022-09-15 13:43:27 -0300

    feat: esqueci de importar os erros :)

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import 'express-async-errors';
+import errorHandler from './middlewares/error';
 import CarRouter from './routes/CarRouter';
 
 const app = express();
@@ -6,4 +8,5 @@ const app = express();
 app.use(express.json());
 app.use('/', CarRouter);
 
+app.use(errorHandler);
 export default app;
